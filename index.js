e503634cbdf8b904eed1dd6e791d88df24f4f7d3
Brigitte Orina <brigitteorina@gmail.com>
2022-08-21 23:38:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const returnFirstTwoDrivers=(drivers) => {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers=(drivers) => {
+    return drivers.slice(drivers.length - 2);
+}
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier=(multiplier) => {
+    return (fare) => {
+          return fare * multiplier;
+    }
+}
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+const selectDifferentDrivers=(drivers, callback) => {
+    return callback(drivers);
+}
